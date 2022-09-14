d404dc17eb7f5b7b3ecd43f5e8108992cd18733a
mrxx0 <chcoutur@student.42.fr>
2022-09-14 11:02:48 +0200

    Add rules for udev + linux coding standard for module + proof

--- assignment_04/usb_keyboard_module.c
+++ assignment_04/usb_keyboard_module.c
@@ -1,18 +1,8 @@
+// SPDX-License-Identifier: GPL-2.0
 #include <linux/module.h>
 #include <linux/kernel.h>
 #include <linux/usb.h>
 #include <linux/hid.h>
-
-int probe(struct usb_interface *intf, const struct usb_device_id *id)
-{
-		printk(KERN_INFO "USB keyboard plugged in.\n");
-		return 0;
-}
-
-void disconnect (struct usb_interface *intf)
-{
-	printk(KERN_INFO "USB keyboard plugged out.\n");
-}
 
 /*
  * Use to support hotplugging for a specific device
@@ -22,38 +12,26 @@ static const struct usb_device_id id_table[] = {
 			USB_INTERFACE_SUBCLASS_BOOT,
 			USB_INTERFACE_PROTOCOL_KEYBOARD) },
 	{}
-};	
+};
 
 /*
  * Allow hotplug and which modules work with what hardware devices
  */
 MODULE_DEVICE_TABLE(usb, id_table);
 
-/*
- * Identifies the USB device driver to usbcore
- */
-static struct usb_driver chcoutur_usb_driver = {
-	.name = "chcoutur_usb_driver",
-	.id_table = id_table,
-	.probe = probe,
-	.disconnect = disconnect,
-};
-
 static int __init usb_init_module(void)
 {
-		printk(KERN_INFO "Hello world!\n");
-		usb_register(&chcoutur_usb_driver);
-		return 0;
+	pr_info("usb_keyboard_module : USB keyboard connected !\n");
+	return 0;
 }
 
 static void __exit usb_cleanup_module(void)
 {
-		printk(KERN_INFO "Cleaning up module.\n");
-		usb_deregister(&chcoutur_usb_driver);
+	pr_info("usb_keyboard_module : USB keyboard disconnected !\n");
 }
 
 module_init(usb_init_module);
 module_exit(usb_cleanup_module);
 MODULE_LICENSE("GPL");
 MODULE_AUTHOR("chcoutur");
-MODULE_DESCRIPTION("A simple module to load a USB keyboard.");
+MODULE_DESCRIPTION("A simple hello world module.");
