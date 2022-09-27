de720188e87783f1e8cb995aad7060d9dd32bb67
mrxx0 <chcoutur@student.42.fr>
2022-09-27 15:54:19 +0200

    Complete mymount module, need to refactor the code

--- assignment_09/mymounts.c
+++ assignment_09/mymounts.c
@@ -10,33 +10,71 @@
 static struct proc_dir_entry *mymounts;
 char path_list[PATH_MAX];
 
+static void get_size(struct mnt_namespace *mnt_ns, int *size)
+{
+	struct mount *mnt;
+	struct path mnt_path;
+	size_t len;
+	size_t maxlen;
+	char *path;
+
+	maxlen = 0;
+	len = 0;
+	path = NULL;
+	list_for_each_entry(mnt, &mnt_ns->list, mnt_list){
+		if (mnt->mnt_id != 1){
+			len = strlen(mnt->mnt_devname);
+			if (len > maxlen)
+				maxlen = len;
+			mnt_path.mnt = &mnt->mnt;
+			mnt_path.dentry = mnt->mnt.mnt_root;
+			path = d_path(&mnt_path, path_list, PATH_MAX);
+			size[0] += strlen(path);
+		}
+	}
+	size[1] = maxlen + 8;
+	size[0] += (size[1] + 1) * (mnt_ns->mounts - 1);
+}
+
 static ssize_t mymounts_list(struct file *file, char __user *str, size_t len, loff_t *offset)
 {
 	// nsproxy is shared by tasks which share all namespaces and we want mnt_namespace
 	struct mnt_namespace *mnt_ns = current->nsproxy->mnt_ns;
 	struct mount *mnt;
 	struct path mnt_path;
-	char *path = kmalloc(PATH_MAX + 1, GFP_KERNEL);
-	if (path == NULL)
-	{
-		kfree(path);
+	char *path;
+	char *output;
+	int cursor;
+	int size[2];
+	ssize_t ret;
+
+	path = NULL;
+	cursor = 0;
+	size[0] = 0; // size[0] is the total len of the print
+	size[1] = 0; // size[1] is the len to padd each result (depends on the longest mnt_devname)
+	ret = 0;
+	// needed in case a mnt_devname is too long
+	get_size(mnt_ns, size);
+	output = kmalloc(size[0], GFP_KERNEL);
+	if (output == NULL)
 		return -ENOMEM;
-	}
 	memset(path_list, 0, sizeof(char) * PATH_MAX);
+	memset(output, 0, size[0]);
 	list_for_each_entry(mnt, &mnt_ns->list, mnt_list) {
-		mnt_path.mnt = &mnt->mnt;
-		mnt_path.dentry = mnt->mnt.mnt_root;
-		if (mnt->mnt_id != 1) { // source : https://lwn.net/Articles/265920/
-		path = d_path(&mnt_path, path_list, PATH_MAX);
-		//pr_info("%s", mnt->mnt_devname);
-		pr_info("%-10s%s\n",mnt->mnt_devname, path);
-		if (IS_ERR(path))
-			return -1;
+			if (mnt->mnt_id != 1) {
+			mnt_path.mnt = &mnt->mnt;
+			mnt_path.dentry = mnt->mnt.mnt_root;
+			path = d_path(&mnt_path, path_list, PATH_MAX);
+			if (IS_ERR(path)) {
+				kfree(output);
+				return -1;
+			}
+			cursor += sprintf(output + cursor, "%-*s%s\n",size[1], mnt->mnt_devname, path);
 		}
-	//	pr_info("%s\n", path);
 	}
-	kfree(path);
-	return 0;
+	ret = simple_read_from_buffer(str, len, offset, output, size[0]) ;
+	kfree(output);
+	return ret;
 }
 
 static const struct proc_ops mymounts_procops = {
@@ -65,3 +103,4 @@ module_exit(cleanup_mymounts);
 MODULE_LICENSE("GPL");
 MODULE_AUTHOR("chcoutur");
 MODULE_DESCRIPTION("A simple mount listing module.");
+
