e66aa2b13d57b554feee5c91dfa79eaec34785fc
mrxx0 <chcoutur@student.42.fr>
2022-09-28 17:53:08 +0200

    Update keyboard module

--- assignment_04/usb_keyboard_module.c
+++ assignment_04/usb_keyboard_module.c
@@ -4,21 +4,6 @@
 #include <linux/usb.h>
 #include <linux/hid.h>
 
-/*
- * Use to support hotplugging for a specific device
- */
-static const struct usb_device_id id_table[] = {
-	{ USB_INTERFACE_INFO(USB_INTERFACE_CLASS_HID,
-			USB_INTERFACE_SUBCLASS_BOOT,
-			USB_INTERFACE_PROTOCOL_KEYBOARD) },
-	{}
-};
-
-/*
- * Allow hotplug and which modules work with what hardware devices
- */
-MODULE_DEVICE_TABLE(usb, id_table);
-
 static int __init usb_init_module(void)
 {
 	pr_info("usb_keyboard_module : USB keyboard connected !\n");
